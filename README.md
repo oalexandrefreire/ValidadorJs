bbe03cc2b45c4f41c34594827d73874b2bc28af8
Alexandre Freire <alexandre.com9@gmail.com>
2021-02-18 17:48:13 -0400

    start

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Af.ValidadorJs
+Validador de CPF
+
+# Install
+npm install validadorjs
+
+# Usage
+```
+import {ValidarCPF} from 'validadorjs';
+
+var cpf = '12345678998';
+ValidarCPF(cpf); // true or false
+```
---
+++ index.js
@@ -0,0 +1,26 @@
+"use strict";
+exports.__esModule = true;
+function ValidarCPF(cpf) {
+    var Soma;
+    var Resto;
+    Soma = 0;
+    if (cpf == "00000000000")
+        return false;
+    for (var i = 1; i <= 9; i++)
+        Soma = Soma + parseInt(cpf.substring(i - 1, i)) * (11 - i);
+    Resto = (Soma * 10) % 11;
+    if ((Resto == 10) || (Resto == 11))
+        Resto = 0;
+    if (Resto != parseInt(cpf.substring(9, 10)))
+        return false;
+    Soma = 0;
+    for (var i = 1; i <= 10; i++)
+        Soma = Soma + parseInt(cpf.substring(i - 1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
+    if ((Resto == 10) || (Resto == 11))
+        Resto = 0;
+    if (Resto != parseInt(cpf.substring(10, 11)))
+        return false;
+    return true;
+}
+exports.ValidarCPF = ValidarCPF;
